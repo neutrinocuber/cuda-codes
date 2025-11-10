5c32ea4f5de4f82251e76b1c227c2a79e4b2dcf0
neutrinocuber <158823481+neutrinocuber@users.noreply.github.com>
2025-11-10 18:46:32 +0530

    Update vector_add.cu

--- vector_add.cu
+++ vector_add.cu
@@ -8,10 +8,33 @@ __global__ void vector_add(int *A, int *B, int *C, int N){
 int main(){
     const int N=10;
     int h_A[N], h_B[N], h_C[N];
-    for (int i=0; i<N;i++){
-        h_A[i]=i;
-        h_B[i]=2*i;
+    h_A=(int*) malloc(N*sizeof(int));
+    h_B=(int*) malloc(N*sizeof(int));
+    h_C=(int*) malloc(N*sizeof(int));
+    if (h_A==NULL){
+        printf("memory not allocated");
+        return 1;
     }
+    h_A[0]=-1;
+    h_A[1]=3;
+    h_A[2]=10;
+    h_A[3]=2;
+    h_A[4]=-20;
+    h_A[5]=2;
+    h_A[6]=7;
+    h_A[7]=7;
+    h_A[8]=6;
+    h_A[9]=-3;
+    h_B[0]=4,
+    h_B[1]=1,
+    h_B[2]=-2,
+    h_B[3]=12,
+    h_B[4]=7, 
+    h_B[5]=-1,
+    h_B[6]=0,
+    h_B[7]=-1,
+    h_B[8]=9,
+    h_B[9]=0;
     int *d_A, *d_B, *d_C;
     cudaMalloc((void**)&d_A,N*sizeof(int));
     cudaMalloc((void**)&d_B,N*sizeof(int));
